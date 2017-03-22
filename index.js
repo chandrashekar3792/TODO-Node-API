fefaaa5b820a60428f52146a4af629c65d8a854c
Chandrashekar <chandrashekar998@gmail.com>
2017-03-23 00:05:09 +0530

    Reading an particular Todo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var bodyParser=require('body-parser');
 var {mongoose}=require('./db/mongoose');
 var {Todo}=require('./models/todos');
 var {User}=require('./models/users');
-
+var {ObjectId}=require('mongodb');
 var app = express();
 app.use(bodyParser.json());
 
@@ -16,6 +16,26 @@ todo.save().then((doc)=>{
     res.send(doc);
   }, (e)=>{
     res.send(e);
+  });
+});
+
+app.get('/todos',(req,res)=>{
+  Todo.find().then((todos)=>{
+    res.send({todos:todos});
+  },(e)=>{
+    res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id',(req,res)=>{
+  var id=req.params.id;
+  if(!ObjectId.isValid(id)){
+    return res.status(404).send({message:'Not Valid Id'});
+  }
+  Todo.findOne({_id:req.params.id}).then((todo)=>{
+    res.send({todo:todo});
+  },(e)=>{
+    res.status(400).send(e);
   });
 });
 
