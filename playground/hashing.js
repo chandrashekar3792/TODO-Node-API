3526b4109da40be2995833c306f5f7e33fb60cd0
Chandrashekar <chandrashekar998@gmail.com>
2017-03-24 01:32:31 +0530

    hashing using SHA256

--- playground/hashing.js
+++ playground/hashing.js
@@ -5,3 +5,22 @@ var hash=SHA256(message).toString();
 
 console.log(`Message:${message}`);
 console.log(`Hash:${hash}`);
+
+var data={
+  id:4
+};
+var token={
+  data,
+  hash:SHA256(JSON.stringify(data)+'somesecret').toString()
+};
+
+// token.data.id=5;
+// token.hash=SHA256(JSON.stringify(token.data)).toString();
+
+var resultHash=SHA256(JSON.stringify(token.data)+'somesecret').toString();
+
+if(resultHash===token.hash){
+  console.log('Data was not Changed');
+}else{
+  console.log('Data was changed. Do not trust');
+}
