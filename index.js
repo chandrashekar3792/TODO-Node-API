1ef34807c28cbdbccc293fe3f5f6d1fc971dda8d
Chandrashekar <chandrashekar998@gmail.com>
2017-03-24 01:19:54 +0530

    Adding user authentication and hashing

--- index.js
+++ index.js
@@ -76,6 +76,16 @@ app.patch('/todos/:id',(req,res)=>{
   });
 });
 
+//POST
+app.post('/users',(req,res)=>{
+  var body=_.pick(req.body,['email','password']);
+  var user= new User(body);
+  user.save().then(()=>{
+    res.send(user);
+  }).catch((e)=>{
+    res.status(400).send(e);
+  });
+});
 app.listen(3000,(err,res)=>{
   if(!err){
     console.log('app is running at port:3000');
--- models/users.js
+++ models/users.js
@@ -1,12 +1,40 @@
 var mongoose=require('mongoose');
-
+var validator=require('validator');
 var User=mongoose.model('User',{
+  // {
+  //   email:'chandrashekar998@gmail.com',
+  //   password:'myPass123',
+  //   tokens:[{
+  //     access:'auth',
+  //     token:'dddweddcedcfeeee'
+  //   }]
+  // },
   email:{
     type:String,
     required:true,
     trim:true,
-    minlength:5
-  }
+    minlength:5,
+    unique:true,
+    validate:{
+      validator:validator.isEmail,
+      message:'{VALUE} is not valid'
+    }
+  },
+  password:{
+    type:String,
+    required:true,
+    minlength:6
+  },
+  token:[{
+    access:{
+      type:String,
+      required:true
+    },
+    token:{
+      type:String,
+      required:true
+    }
+  }]
 });
 
 module.exports={
